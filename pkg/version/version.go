e5c8de0ab5ffedf47c8756645c5513e7f01bbdc7
Sergio Rubio <rubiojr@rbel.co>
2025-10-05 15:17:16 +0200

    Bump version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version represents the current version of Ergs
-const Version = "3.0.1"
+const Version = "3.1.0"
 
 // BuildVersion returns the version string for display
 func BuildVersion() string {
